769e700632a9f84d81d4520a97832802c0ac9113
Daniele Marconi <daniele.marconi@desy.de>
2015-04-11 14:34:05 +0200

    Added enemy prototype assets

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public float enemySpeed;
+	public float enemyMaxHealth;
+
+	private float enemyHealth;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.Find("Player");
+		enemyHealth = enemyMaxHealth;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float zrotation = Mathf.Atan2(player.transform.position.y - transform.position.y, player.transform.position.x - transform.position.x ) * Mathf.Rad2Deg - 90;
+		transform.eulerAngles = new Vector3 (0,0,zrotation);
+
+		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * enemySpeed);
+
+	}
+}
